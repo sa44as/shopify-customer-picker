59356c263d90ae5bae87572a53119fc74a524f89
Sargis Gevorgyan <sa44as44@gmail.com>
2023-05-12 15:48:09 +0400

    restResources version to 2023-01

--- web/services/shopify.service/index.js
+++ web/services/shopify.service/index.js
@@ -1,7 +1,7 @@
 import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
-import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
+import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
 // to do, Move DB_CONNECTION_STRING and DB_NAME to .env before switching to the production mode. https://stax-development.atlassian.net/jira/software/projects/LP/boards/48?selectedIssue=LP-17
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://loyalty-user:346u1F59GLjr7f2X@loyalty-program-db-cluster-d5440995.mongo.ondigitalocean.com/loyalty-program-database?tls=true&authSource=admin&replicaSet=loyalty-program-db-cluster';
 
