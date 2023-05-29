b767696f9d2b9827574dc6e40d628f645cb0eea8
Sargis Gevorgyan <sa44as44@gmail.com>
2023-05-29 16:18:14 +0400

    Version updates

--- web/index.js
+++ web/index.js
@@ -79,10 +79,10 @@ app.use("/api/internal/*", shopify.validateAuthenticatedSession()); // to do, co
 
 app.get("/api/internal/products/count", async (_req, res) => {
   console.log('res.locals.shopify.session: ', res.locals.shopify.session);
-  const countData = await shopify.api.rest.Product.count({
-    session: res.locals.shopify.session,
-  });
-  res.status(200).send(countData);
+  // const countData = await shopify.api.rest.Product.count({
+  //   session: res.locals.shopify.session,
+  // });
+  res.status(200).send('countData');
 });
 
 app.get("/api/internal/products/create", async (_req, res) => {
