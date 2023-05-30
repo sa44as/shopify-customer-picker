a426aec51d23054dcb4bb41569e178f7d91e3c4f
Sargis Gevorgyan <sa44as44@gmail.com>
2023-05-30 18:34:19 +0400

    added customer controller

--- web/controllers/customer.controller.js
+++ web/controllers/customer.controller.js
@@ -3,20 +3,22 @@ import { orderService } from "../services/map.js";
 const customerController = {
   getPointsBalance: async (req, res) => {
     const response = await orderService.aggregate(
-      {
-        $match: {
-          shopify_session: req.shopifySession._id,
-          shopify_customer_id: req.params.shopify_customer_id,
-        }
-      },
-      {
-        $group: {
-          _id: null,
-          points_balance: {
-            $sum: "$points"
+      [
+        {
+          $match: {
+            shopify_session: req.shopifySession._id,
+            shopify_customer_id: req.params.shopify_customer_id,
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            points_balance: {
+              $sum: "$points"
+            }
           }
         }
-      }
+      ]
     );
 
     console.log("getPointsBalance.resposne: ", response);
