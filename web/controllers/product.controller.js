f0ff1755eb1a079f1cbebb128d94b8487579377d
Sargis Gevorgyan <sa44as44@gmail.com>
2023-05-30 20:25:53 +0400

    GET /api/product/is_reward/:shopify_product_id API request fix

--- web/controllers/product.controller.js
+++ web/controllers/product.controller.js
@@ -5,18 +5,13 @@ const productController = {
     const response = await configurationService.find(
       {
         shopify_session: req.shopifySession._id,
-        reward_products: {
-          $elemMatch: {
-            shopify_product_id: req.params.shopify_product_id,
-          }
-        },
+        "reward_products.shopify_product_id": req.params.shopify_product_id,
       }
     );
 
     const isRewardProduct = Array.isArray(response) && response.length;
     const rewardProductConfiguration = isRewardProduct ? response[0].reward_products.filter((reward_product) => reward_product.shopify_product_id == req.params.shopify_product_id)[0] : null;
-    // debugger
-    console.log("response:", response);
+
     return res.json(
       {
         is_reward_product: isRewardProduct,
