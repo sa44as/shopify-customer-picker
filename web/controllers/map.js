fe0fabf5f97fb80eb97b159badcb99ce2edd7262
Sargis Gevorgyan <sa44as44@gmail.com>
2023-05-30 19:17:57 +0400

    GET /api/product/is_reward/:shopify_product_id API request fix

--- web/controllers/map.js
+++ web/controllers/map.js
@@ -1,2 +1,3 @@
 export { orderController } from "./order.controller.js";
 export { customerController } from "./customer.controller.js";
+export { productController } from "./product.controller.js";
