85e9eede88be55ee51ba4da8246f9bcd2a2a9dd5
Sargis Gevorgyan <sa44as44@gmail.com>
2023-06-02 01:56:00 +0400

    Reward products setup interface, not final result

--- web/frontend/components/index.js
+++ web/frontend/components/index.js
@@ -1,4 +1,5 @@
 export { ProductsCard } from "./ProductsCard";
 export { QRCodeForm } from "./QRCodeForm";
 export { QRCodeIndex } from "./QRCodeIndex";
+export { RewardProductsIndex } from "./RewardProduictsIndex";
 export * from "./providers";
